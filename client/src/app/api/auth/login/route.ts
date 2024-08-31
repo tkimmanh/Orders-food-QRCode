c551661f9aafbd470a900c50f130c96de71636cb
tkimmanh <tkmanh110329@gmail.com>
2024-09-01 01:55:47 +0700

    route handler

---
+++ client/src/app/api/auth/login/route.ts
@@ -0,0 +1,44 @@
+import { authApiRequest } from "@/app/apiRequest/auth";
+import { HttpError } from "@/lib/http";
+import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  const body = (await request.json()) as LoginBodyType;
+  const cookieStore = cookies();
+  try {
+    const { payload } = await authApiRequest.severLogin(body);
+    const { accessToken, refreshToken } = payload.data;
+    /*
+    decode để lấy được thời điểm token hết hạn , 
+    */
+    const decodedAccessToken = jwt.decode(accessToken) as { exp: number };
+    const decodedRefreshToken = jwt.decode(accessToken) as { exp: number };
+    cookieStore.set("accessToken", accessToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax", // giảm rủi ro CSRF
+      secure: true, // chỉ gửi cookie qua https
+      expires: decodedAccessToken.exp * 1000, // thời gian hết hạn của token
+    });
+    cookieStore.set("refreshToken", refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return Response.json(error.payload, {
+        status: error.status,
+      });
+    } else {
+      Response.json({
+        message: "Đã có lỗi xảy ra",
+      });
+    }
+  }
+}
---
+++ client/src/app/apiRequest/auth.ts
@@ -0,0 +1,17 @@
+import http from "@/lib/http";
+import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+
+class AuthApiRequest {
+  // gọi tới server login
+  async severLogin(body: LoginBodyType) {
+    return await http.post<LoginResType>("/auth/login", body);
+  }
+  // gọi tới nextjs server
+  async clientLogin(body: LoginBodyType) {
+    return await http.post("/api/auth/login", body, {
+      baseUrl: "",
+    });
+  }
+}
+
+export const authApiRequest = new AuthApiRequest();

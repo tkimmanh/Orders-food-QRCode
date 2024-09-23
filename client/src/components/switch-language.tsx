c0e65aff560b119a8dae8a74c7369fa433377326
tkimmanh <tkmanh110329@gmail.com>
2024-09-23 15:55:26 +0700

    chuyển đổi ngôn ngữ

--- client/src/components/switch-language.tsx
+++ client/src/components/switch-language.tsx
@@ -7,19 +7,34 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Locale, locales } from "@/config";
-import { setUserLocale } from "@/services/locale";
 import { useLocale, useTranslations } from "next-intl";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
 
-import React, { use } from "react";
+import React from "react";
 
 const SwitchLanguage = () => {
   const t = useTranslations("SwitchLanguage");
   const local = useLocale();
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const params = useParams();
   return (
     <>
       <Select
         value={local}
-        onValueChange={(value) => setUserLocale(value as Locale)}
+        onValueChange={(value) => {
+          const locale = params.locale as Locale; // lấy giá trị locale từ params 'en' hoặc 'vi'
+          const newPatchName = pathname.replace(`/${locale}`, `/${value}`); // thay thế giá trị locale cũ bằng giá trị locale mới
+          const fullUrl = `${newPatchName}?${searchParams.toString()}`; // tạo url mới /en/abc?name=abc
+          router.replace(fullUrl); // thay đổi url
+          router.refresh(); // refresh lại trang 
+        }}
       >
         <SelectTrigger className="w-[180px]">
           <SelectValue placeholder={t("title")} />
